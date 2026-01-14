5f43bfb53420d1c583be4390e734c80b10072448
Alpay Özer <alpayozer7@gmail.com>
2026-01-14 15:43:52 +0300

    render weight box at clicked position

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,6 @@
+import { PLANK_LENGTH } from "./constants.js";
+import { containerDisplay } from "./dom.js";
+import { state } from "./state.js";
 import { generateRandomWeight, renderBox, updateValues } from "./ui.js";
 
 const init = () => {
@@ -5,10 +8,33 @@ const init = () => {
 
   generateRandomWeight();
   updateValues(10, 5, 20);
-  renderBox({ id: 3, weight: 3, styleLeft: "90px" });
-  renderBox({ id: 7, weight: 7, styleLeft: "250px" });
-  renderBox({ id: 9, weight: 9, styleLeft: "330px" });
-  renderBox({ id: 10, weight: 10, styleLeft: "370px" });
+
+  containerDisplay.addEventListener("click", (e) => {
+    const containerArea = containerDisplay.getBoundingClientRect();
+    const balancePoint = containerArea.left + containerArea.width / 2;
+    const boxDistance = e.clientX - balancePoint;
+    const scale = containerArea.width / PLANK_LENGTH;
+    const scaledDistance = boxDistance / scale;
+    const boxPoint = PLANK_LENGTH / 2 + scaledDistance;
+
+    let nextWeight = state.nextWeight;
+
+    if (nextWeight <= 0) {
+      nextWeight = Math.floor(Math.random() * 10) + 1;
+      state.nextWeight = nextWeight;
+      nextWeightDisplay.textContent = `${state.nextWeight}kg`;
+    }
+
+    const newBox = {
+      id: Date.now(),
+      weight: nextWeight,
+      styleLeft: `${boxPoint}px`,
+    };
+
+    renderBox(newBox);
+    state.objects.push(newBox);
+    generateRandomWeight();
+  });
 };
 
 document.addEventListener("DOMContentLoaded", init);
